aa0f1a2ac3e6a9dd283ba98a66b3ab80a448f0d4
J Montiel <jmaidana18@gmail.com>
2020-03-03 15:36:51 -0300

    Create cashier.py.py
    Initial design for a Cashier App.

---
+++ cashier.py.py
@@ -0,0 +1,52 @@
+class product():
+    
+    def __init__(self, desc,quality, origin, price,ip_boolean,ex_boolean):
+        
+        self.desc = desc
+        self.quality = quality
+        self.origin = origin
+        self.price = price
+        self.tax_rate = 0.1
+        self.import = ip_boolean
+        self.exen = ex_boolean
+        self.tax = 
+    def cal_tax():
+        if self_import:
+            self.tax * 0.5
+        elif self.exen:
+            pass
+        else:
+            self.product_rate*
+    def get_tax():
+        return (self.tax)
+
+    # getter_tax()
+    # getter_price()
+    
+# alternative: generate a random Id number
+# persist object with pickle.
+
+class register(quantity, product):
+    def __init__(self,quantity,product):
+        self.quantity = quantity
+        self.product = product
+    
+    def payment():
+        self.total_tax = self.total + product.get_tax()
+        self.total_pay = self.total_pay + product.get_price()
+        # variables globales
+def printer():
+    variables 
+    # cerar variables totales de register()
+    
+# rutina ingrese productos CRUD Database?
+libro = new product()
+peli = new product()
+barrita = new product()
+
+# rutina registro de productos.
+while n <> 0 :
+    print ("ingrese 0 (cero) para detener el ciclo de registro")
+    
+    input_1 = raw_input()
+    mandar a register(1,libro)
